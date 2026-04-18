1dc4c610977f813ddb105ef3ec31a4ca00f540dc
nolesiak <n.olesiak@gmail.com>
2026-04-18 21:57:23 +0200

    Init front

--- src/backend/Mavrynt.AppHost/AppHost.cs
+++ src/backend/Mavrynt.AppHost/AppHost.cs
@@ -1,3 +1,30 @@
+const string apiName = "api";
+const string adminApiName = "admin-api";
+const string webAppName = "web";
+const string adminWebAppName = "admin-web";
+const string landingAppName = "landing";
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var api = builder.AddProject<Projects.Mavrynt_Api>(apiName)
+    .WithExternalHttpEndpoints();
+var adminApi = builder.AddProject<Projects.Mavrynt_AdminApp>(adminApiName)
+    .WithExternalHttpEndpoints();
+
+var web = builder.AddViteApp(webAppName, "../../frontend/mavrynt-web")
+    .WithNpmPackageInstallation()
+    .WithReference(api)
+    .WaitFor(api)
+    .WithExternalHttpEndpoints();
+
+var adminWeb = builder.AddViteApp(adminWebAppName, "../../frontend/mavrynt-admin")
+    .WithNpmPackageInstallation()
+    .WithReference(adminApi)
+    .WaitFor(adminApi)
+    .WithExternalHttpEndpoints();
+
+var landing = builder.AddViteApp(landingAppName, "../../frontend/mavrynt-landing")
+    .WithNpmPackageInstallation()
+    .WithExternalHttpEndpoints();
 
 builder.Build().Run();
